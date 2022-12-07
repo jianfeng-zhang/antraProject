c2fa5aeedefccea0ba15368e14cdb7d1c5124e3c
jianfeng-zhang <91287732+jianfeng-zhang@users.noreply.github.com>
2022-12-06 23:58:03 -0600

    Add files via upload

---
+++ homework/restTemplate/src/main/java/com/example/resttemplate/domain/entity/Data.java
@@ -0,0 +1,16 @@
+package com.example.resttemplate.domain.entity;
+
+@lombok.Data
+public class Data {
+
+
+    private Integer id ;
+
+    private String employee_name;
+
+    private Integer employee_salary;
+
+    private Integer employee_age;
+
+    private  String profile_image;
+}
---
+++ homework/restTemplate/src/main/java/com/example/resttemplate/respository/impl/EmployeeCustomRepoImpl.java
@@ -0,0 +1,17 @@
+package com.example.resttemplate.respository.impl;
+
+
+import com.example.resttemplate.respository.EmployeeCustomRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+
+@Repository
+public class EmployeeCustomRepoImpl implements EmployeeCustomRepo {
+
+
+
+
+}
+
