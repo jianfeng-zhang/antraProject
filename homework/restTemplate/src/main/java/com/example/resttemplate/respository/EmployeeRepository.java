ebcb02418a1e3300eade10caeaf17f1649a1b35b
jianfeng-zhang <91287732+jianfeng-zhang@users.noreply.github.com>
2022-12-06 23:59:39 -0600

    Delete homework/restTemplate directory

--- homework/restTemplate/src/main/java/com/example/resttemplate/respository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.resttemplate.respository;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends  EmployeeCustomRepo {
-}
