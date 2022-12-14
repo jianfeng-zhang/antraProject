ae2538457c093e14f9e1a9b1def6fd19cd7c092a
jianfeng-zhang <91287732+jianfeng-zhang@users.noreply.github.com>
2022-12-13 23:56:44 -0600

    Delete university-master_lastest directory

--- university-master_lastest/src/main/java/com/example/university/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.university.exception;
-
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- university-master_lastest/src/main/java/com/example/university/respository/RelationshipRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.university.respository;
-
-
-import com.example.university.domain.reslationship.Relationship;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RelationshipRepository extends JpaRepository<Relationship, String>, RelationshipCustomRepo{
-}
